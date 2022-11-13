05b153c15f4a63f8c2b9fbcedb2b2b110485a4ca
Dejan Stosic <dejan7stosic@gmail.com>
2022-11-13 17:50:24 +0100

    added more info about legacy mode

--- docs/pages/index.vue
+++ docs/pages/index.vue
@@ -23,16 +23,17 @@
         <p class="text-slategray-600" style="font-size: 1.25rem">
           EtherUI is a modern CSS UI kit which tries to be very simple and
           lightweight, yet powerful and extensible. The package ships with
-          <span class="font-bold">no JavaScript</span> and there are no complicated
-          build steps, using EtherUI in your project is incredibly simple, you could
-          be using it within few seconds!
+          <span class="font-bold">no JavaScript</span> and there are no
+          complicated build steps, using EtherUI in your project is incredibly
+          simple, you could be using it within few seconds!
         </p>
         <hr />
         <p>
-          There are multiple ways to use EtherUI, as the UI kit is just a single CSS
-          file. <br />
-          EtherUI is a combination of semantic CSS classes (e.g. btn), but it also
-          ships with a limited (sensible) amount utility classes. Utility classes
+          There are multiple ways to use EtherUI, as the UI kit is just a single
+          CSS file. <br />
+          EtherUI is a combination of semantic CSS classes (e.g. btn), but it
+          also ships with a limited (sensible) amount utility classes. Utility
+          classes
           <span class="font-bold">are named exactly the same</span> as in
           <a href="https://tailwindcss.com/" target="_blank">TailwindCSS.</a>
         </p>
@@ -40,8 +41,8 @@
         The simplest and fastest way is to grab the CSS file (<a
           class="font-bold"
           href="https://github.com/ether-ui/ether-ui/blob/main/dist/ether-ui.min.css"
-      >ether-ui.min.css</a
-      >) and include it in your HTML, along with the fonts:
+          >ether-ui.min.css</a
+        >) and include it in your HTML, along with the fonts:
         <pre><code v-highlight class="html">&lt;!DOCTYPE html>
 &lt;html data-ether-theme="ether-light">
   &lt;head>
@@ -63,7 +64,6 @@
         <code>&lt;html></code> tag!
         <pre><code v-highlight class="html" >&lt;html data-ether-theme="ether-light"></code></pre>
 
-
         <h5 class="mt-8 mb-2 font-bold" id="tailwind">
           Tailwind Compatibility Mode
         </h5>
@@ -71,38 +71,54 @@
         <NuxtLink to="/helpers">utility classes</NuxtLink>, but not with all
         possible variants and combinations, as
         <a href="https://tailwindcss.com/">TailwindCSS</a>. If you feel like you
-        need the flexiblity of all utility classes, i wholeheartedly recommend using
-        TailwindCSS <span class="font-bold">alongside</span> EtherUI. In fact,
-        EtherUI is built with this in mind! In order to eliminate duplicated helper
-        classes and minimize you end build file, you should include
+        need the flexiblity of all utility classes, i wholeheartedly recommend
+        using TailwindCSS <span class="font-bold">alongside</span> EtherUI. In
+        fact, EtherUI is built with this in mind! In order to eliminate
+        duplicated helper classes and minimize you end build file, you should
+        include
         <a
-            href="https://github.com/ether-ui/ether-ui/blob/main/dist/ether-ui.tw-comp.min.css"
-        >ether-ui.tw-comp.min.css</a
+          href="https://github.com/ether-ui/ether-ui/blob/main/dist/ether-ui.tw-comp.min.css"
+          >ether-ui.tw-comp.min.css</a
         >
         instead of the original EtherUI file
 
         <h5 class="mt-8 mb-2 font-bold" id="legacy">Legacy Mode</h5>
-        EtherUI acknowledges the struggles of real world legacy web apps. Want to
-        use it in your 12 year old legacy back-office app? No worries, EtherUI got
-        you covered!
+        EtherUI acknowledges the struggles of real world legacy web apps. Want
+        to use it in your 12 year old legacy back-office app? No worries,
+        EtherUI got you covered!
         <br />
-        Make sure to create a <code>etherui.config.js</code> file in your project's
-        root with the following contents:
+        Make sure to create a <code>etherui.config.js</code> file in your
+        project's root with the following contents:
         <pre><code v-highlight class="scss" v-html="code[3]"></code></pre>
         Then run
         <pre><code v-highlight class="bash">npm etherui</code></pre>
         And you will receive built css files which you should use:
         <pre><code v-highlight class="txt" v-html="code[4]"></code></pre>
 
-
-        When you use one of these file,
-        <span class="font-bold">all classes</span> will have the
-        <code>mycss-</code>
+        When you use one of these files,
+        <span class="font-bold">all EtherUI classes</span> will have the
+        <code>ether-</code>
         Literally all of EtherUI css classes will have that prefix, e.g.
-        <code>mycss-row</code>, <code>mycss-col</code> etc.<br />
-        Prefix itself is, of course, customizable, you can use any other prefix in
-        your config file. If you would like to configure EtherUI further, take a
-        look at the <NuxtLink to="/configuration">configuration page</NuxtLink>.
+        <code>.ether-row</code>, <code>.ether-col</code> etc. Prefix itself is,
+        of course, customizable, you can use any other prefix in your config
+        file. <br />
+        Like for all other modes, you'll need to add the
+        <pre><code v-highlight class="html" >&lt;html data-ether-theme="ether-light"></code></pre>
+        to your HTML. This mode does not set any <code>body {}</code> styles,
+        meaning that no font-family is specified globally to not mess up with
+        your existing app font-family. You can try using EtherUI with your own
+        font, or alternatively you can add this somewhere in your CSS
+        <pre><code v-highlight class="css">[class^="ether-"] {
+  font-family: "Public Sans", sans-serif;
+  font-weight: 400;
+  line-height: 160%;
+  letter-spacing: 0.005em;
+  color: var(--base-content);
+}</code></pre>
+        And now only EtherUI elements will have the currently recommended Public
+        Sans font.<br />
+        If you would like to configure EtherUI further, take a look at the
+        <NuxtLink to="/configuration">configuration page</NuxtLink>.
       </div>
     </template>
   </e-page>
@@ -111,7 +127,7 @@
 <script>
 import EPage from "../components/EPage";
 export default {
-  components: {EPage},
+  components: { EPage },
   data() {
     return {
       code: [
